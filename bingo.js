0bb1d5cb28d3b35d24bac5fca40b20db50c477c8
よし <81900319+yoshi-homare@users.noreply.github.com>
2021-06-01 21:29:16 +0900

    Add files via upload

--- bingo.js
+++ bingo.js
@@ -1,7 +1,9 @@
 "use strict";
 
 {
-  const createColumn = (col) => {
+  // 1-15,16-30,31-45,46-60,61-75の数字を取得。
+  // 1行に1-15を5列にランダムに配置。
+  function createColumn(col) {
     const source = [];
     for (let i = 0; i < 15; i++) {
       source[i] = i + 1 + 15 * col;
@@ -14,7 +16,8 @@
     return column;
   };
 
-  const createColumns = () => {
+  // 上のランダムに配置した行をそれぞれの数字5行分配置し真ん中にFREEを挿入。
+  function createColumns() {
     const columns = [];
     for (let i = 0; i < 5; i++) {
       columns[i] = createColumn(i);
@@ -23,7 +26,8 @@
     return columns;
   };
 
-  const renderBingo = (columns) => {
+  // 各行にtrタグを作成し各行にtdタグを作成し中に上のランダムの数字を行と列を入れ替えて挿入しtrタグにtdタグを挿入しhtmlのtbodyタグを取得しそこにtrタグを挿入。
+  function renderBingo(columns) {
     for (let row = 0; row < 5; row++) {
       const tr = document.createElement("tr");
       for (let col = 0; col < 5; col++) {
